90026065160e1bd620579f38294dae0ee2166153
SuperGoodMike <mike_mitchell@outlook.com>
2025-02-02 11:00:34 -0600

    Update weather.php
    Adding units

--- ip-api/includes/weather.php
+++ ip-api/includes/weather.php
@@ -9,14 +9,19 @@ function get_weather($lat, $lon) {
 
     return [
         'temperature' => $data['current_weather']['temperature'] ?? null,
+        'temperature_unit' => $data['current_weather_units']['temperature'] ?? null,
         'humidity' => $data['hourly']['relativehumidity_2m'][0] ?? null,
         'cloudcover' => $data['hourly']['cloudcover'][0] ?? null,
         'windspeed' => $data['current_weather']['windspeed'] ?? null,
+        'windspeed_unit' => $data['current_weather_units']['windspeed'] ?? null,
         'winddirection' => $data['current_weather']['winddirection'] ?? null,
+        'winddirection_unit' => $data['current_weather_units']['winddirection'] ?? null,
         'is_day' => $data['current_weather']['is_day'] ?? null,
         'weathercode' => $data['current_weather']['weathercode'] ?? null,
         'pressure' => $data['hourly']['pressure_msl'][0] ?? null,
-        'precipitation' => $data['hourly']['precipitation'][0] ?? null
+        'pressure_unit' => $data['current_weather_units']['pressure_msl'] ?? null,
+        'precipitation' => $data['hourly']['precipitation'][0] ?? null,
+        'precipitation_unit' => $data['current_weather_units']['precipitation'] ?? null
     ];
 }
 ?>
